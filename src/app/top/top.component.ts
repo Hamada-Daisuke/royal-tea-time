a115d6904d5867ec48400ca5558d6e7496f26c01
Hamada-Daisuke <hamada.daisuke.0121@gmail.com>
2024-01-14 16:12:43 +0900

    add: 【トップページ】コーディング中間コミット　ヘッダー・フッター・パンくず・お知らせコンポーネントは未作成

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonComponentModule } from '../common/common-component.module';
 
 @Component({
-    selector: 'app-top',
-    standalone: true,
-    templateUrl: './top.component.html',
-    styleUrl: './top.component.scss',
-    imports: [CommonComponentModule]
+  selector: 'app-top',
+  standalone: true,
+  templateUrl: './top.component.html',
+  styleUrl: './top.component.scss',
+  imports: [CommonComponentModule]
 })
 export class TopComponent {
-
+  
 }
